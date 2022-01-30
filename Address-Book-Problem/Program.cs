dc72403753b3542fac95de0fa52949d0d9af1c8b
Lenovo <somusp75@gmail.com>
2022-01-30 20:09:23 +0530

    Uc for Creating Contact WIth Fields

--- Address-Book-Problem/Program.cs
+++ Address-Book-Problem/Program.cs
@@ -7,6 +7,8 @@ namespace Address_Book_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("------------Welcome To Adderess Book Program-------------");
+            Address_Book_Problem.AddressBook.GetContact();
+            Address_Book_Problem.AddressBook.ListContact();
         }
     }
 }
