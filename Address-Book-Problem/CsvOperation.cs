ccf42615d0fb433ec2e936d9d4f59f448f36281d
Lenovo <somusp75@gmail.com>
2022-02-07 17:14:29 +0530

    read and write csv operation added

---
+++ Address-Book-Problem/CsvOperation.cs
@@ -0,0 +1,61 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Address_Book_Problem
+{
+    class CsvOperation
+    {
+        public static void CSVOperation(Dictionary<string, List<AddressBook>> addressbooknames)
+        {
+            // Csv File path in a string
+            string csvFilePath = @"D:\VisualStudio\repos\Address-Book-Problem\Address-Book-Problem\DataCsv.csv";
+            File.WriteAllText(csvFilePath, string.Empty);
+            // over Dictionary Values
+            foreach (KeyValuePair<string, List<AddressBook>> kvp in addressbooknames)
+            {
+                //Open file in Append Mode for adding List elements
+                using var stream = File.Open(csvFilePath, FileMode.Append);
+                using var writer = new StreamWriter(stream);
+                using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+                //Iterate over each value
+                foreach (var value in kvp.Value)
+                {
+                    //Create List to add Records
+                    List<AddressBook> list = new List<AddressBook>();
+                    list.Add(value);
+                    //Write List to CSV File
+                    csvWriter.WriteRecords(list);
+                }
+                //Print a newline
+                csvWriter.NextRecord();
+            }
+            //Reading a Csv File
+            Console.WriteLine("Reading A Csv File And Contents Of AddressBook");
+            Console.WriteLine("-----------------------------------------------------------------");
+            using (var reader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                //Get all records from Csv File
+                var records = csv.GetRecords<AddressBook>().ToList();
+
+                foreach (AddressBook member in records)
+                {
+                    //skip Headers in Csv File
+                    if (member.firstName == "firstName")
+                    {
+                        Console.WriteLine("\n");
+                        continue;
+                    }
+                    //Convert each Value to string and Print to Console
+                    Console.WriteLine(member.ToString());
+                }
+
+            }
+        }
+    }
+}
--- Address-Book-Problem/FileIo.cs
+++ Address-Book-Problem/FileIo.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Address_Book_Problem
 {
      public class FileIo
     {
+        const string filepath = @"D:\VisualStudio\repos\Address-Book-Problem\Address-Book-Problem\File.txt";
+
+        //Write content to file
+        public static void GetDictionary(Dictionary<string, List<AddressBook>> addressbooknames)
+        {
+            //Reset the file as empty
+            File.WriteAllText(filepath, string.Empty);
+            //Iterate over each AddressBook in Dictionary
+            foreach (KeyValuePair<string, List<AddressBook>> kvp in addressbooknames)
+            {
+                //Append key in file
+                File.AppendAllText(filepath, "Address Book: " + kvp.Key + Environment.NewLine);
+                foreach (var member in kvp.Value)
+                {
+                    //Append Values or Contacts in file
+                    File.AppendAllText(filepath, member.ToString() + Environment.NewLine);
+                }
+                //Enter a new line to File
+                File.AppendAllText(filepath, Environment.NewLine);
+                Console.WriteLine("----------The Content written in the file----------");
+                //Read content present in File
+                ReadAddressBook();
+            }
+
+        }
+        public static void ReadAddressBook()
+        {
+            try
+            {
+                //Read All content of File
+                string[] text = File.ReadAllLines(filepath);
+                foreach (var mem in text)
+                    Console.WriteLine(mem);
+                Console.WriteLine("\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
